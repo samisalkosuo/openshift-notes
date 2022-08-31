23295d83b260d6fdf8c7f237b02744879754c782
Sami Salkosuo <samisalkosuo@users.noreply.github.com>
2022-08-31 13:41:41 +0300

    updated

--- install-ng/dnf-install.sh
+++ install-ng/dnf-install.sh
@@ -13,7 +13,8 @@ function installPrereqs
     #prereq packages
     echo "Installing prereq packages..."
     echo "enabling Extra Packages for Enterprise Linux..."
-    dnf -y install epel-release
+    #dnf -y install epel-release
+    dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
     dnf -y install --enablerepo=epel-testing $OMG_PREREQ_PACKAGES
 
     echo "Installing prereq packages...done."
